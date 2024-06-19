a2b5f79ce43be425efe9efb09b476e00462f3ddb
Juan Pablo Lassala <jpablolassala@gmail.com>
2024-06-19 18:28:36 -0300

    Add alquileres

---
+++ src/Alquileres/alquiler.controller.ts
@@ -0,0 +1,21 @@
+import { Controller, Get } from '@nestjs/common';
+
+@Controller('alquileres')
+export class AlquilerController {
+  constructor() {}
+
+  @Get()
+  async getAll() {
+    return 'getAll';
+  }
+
+  @Get(':id')
+  async getOne() {
+    return 'getOne';
+  }
+
+  @Get('/producto/:id')
+  async getAlquileresByProducto() {
+    return 'getAlquileresByProducto';
+  }
+}

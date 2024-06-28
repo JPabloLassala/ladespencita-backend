fd4bacd3a8d12079dcaacdfb039d19151ff80e58
Juan Pablo Lassala <jpablolassala@gmail.com>
2024-06-28 16:08:38 -0300

    Add Global Exception Filter

--- src/main.ts
+++ src/main.ts
@@ -1,8 +1,11 @@
-import { NestFactory } from '@nestjs/core';
+import { HttpAdapterHost, NestFactory } from '@nestjs/core';
 import { AppModule } from './App';
+import { AllExceptionsFilter } from './App/exception.filter';
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
+  const httpAdapter = app.get(HttpAdapterHost);
+  app.useGlobalFilters(new AllExceptionsFilter(httpAdapter));
   await app.listen(3000);
   console.log(`Application is running on: ${await app.getUrl()}`);
 }

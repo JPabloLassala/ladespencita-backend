38f5fd11afcbe502c02887a4cdc9e278997d21c0
Juan Pablo Lassala <jpablolassala@gmail.com>
2024-07-04 16:16:26 -0300

    Add static photo module

--- src/App/app.module.ts
+++ src/App/app.module.ts
@@ -3,15 +3,17 @@ import { AppService } from "./app.service";
 import { ProductoModule } from "src/Productos";
 import { ServeStaticModule } from "@nestjs/serve-static";
 import { join } from "path";
+import { AlquilerModule } from "src/Alquileres/alquiler.module";
+
+const getPhotoModule = () => {
+  return ServeStaticModule.forRoot({
+    rootPath: join(__dirname, "..", "..", "..", "images"),
+    serveRoot: "/images",
+  });
+};
 
 @Module({
-  imports: [
-    ProductoModule,
-    ServeStaticModule.forRoot({
-      rootPath: join(__dirname, "..", "..", "..", "images"),
-      serveRoot: "/images",
-    }),
-  ],
+  imports: [ProductoModule, AlquilerModule, getPhotoModule()],
   controllers: [],
   providers: [AppService],
 })
--- src/App/index.ts
+++ src/App/index.ts
@@ -1,3 +1,2 @@
-export * from "./app.controller";
 export * from "./app.module";
 export * from "./app.service";

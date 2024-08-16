394cc16a5cf592bb62b00ad65e2b9d806a27d2d3
Juan Pablo Lassala <jpablolassala@gmail.com>
2024-08-16 17:43:23 -0300

    Update imports

--- src/App/app.module.ts
+++ src/App/app.module.ts
@@ -1,9 +1,9 @@
 import { Module } from "@nestjs/common";
 import { AppService } from "./app.service";
-import { ProductoModule } from "src/Productos";
+import { ProductoModule } from "src/Producto";
 import { ServeStaticModule } from "@nestjs/serve-static";
 import { join } from "path";
-import { AlquilerModule } from "src/Alquileres/alquiler.module";
+import { AlquilerModule } from "src/Alquiler/alquiler.module";
 
 const getPhotoModule = () => {
   return ServeStaticModule.forRoot({

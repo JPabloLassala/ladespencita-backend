7d7ee32c09dd45666720bfff2cdf5f70ed5abc6b
Juan Pablo Lassala <jpablolassala@gmail.com>
2024-08-30 18:09:22 -0300

    Make seed point to pg

--- seeds/run_seeds.ts
+++ seeds/run_seeds.ts
@@ -1,16 +1,20 @@
 import { exit } from "process";
 import { seed_Alquileres } from "./seed_alquileres";
 import { seed_Products } from "./seed_productos";
-import { ProductoSchema } from "src/Producto";
+import { Sequelize } from "sequelize-typescript";
 import { AlquilerSchema } from "src/Alquiler";
 import { AlquilerProductoSchema } from "src/AlquilerProducto";
-import { Sequelize } from "sequelize-typescript";
+import { ProductoSchema } from "src/Producto";
 
 async function run_seeds() {
   const sequelize = new Sequelize({
-    dialect: "sqlite",
-    storage: "ladespen.sqlite",
-    models: [ProductoSchema, AlquilerSchema, AlquilerProductoSchema],
+    dialect: "postgres",
+    database: "ladespensita",
+    host: process.env.DB_HOST,
+    port: 5432,
+    username: process.env.DB_USER,
+    password: process.env.DB_PASS,
+    models: [AlquilerSchema, AlquilerProductoSchema, ProductoSchema],
   });
 
   await sequelize.sync({ force: true });
--- src/Database/database.provider.ts
+++ src/Database/database.provider.ts
@@ -2,11 +2,14 @@ import { Sequelize } from "sequelize-typescript";
 import { Provider } from "@nestjs/common";
 import { DB_CONNECTION } from "src/constants";
 import { ProductoSchema } from "src/Producto";
+import { AlquilerSchema } from "src/Alquiler";
+import { AlquilerProductoSchema } from "src/AlquilerProducto";
 
 export const databaseProviders: Provider[] = [
   {
     provide: DB_CONNECTION,
     useFactory: async () => {
+      console.log(process.env.DB_HOST);
       const sequelize = new Sequelize({
         dialect: "postgres",
         database: "ladespensita",
@@ -15,7 +18,7 @@ export const databaseProviders: Provider[] = [
         username: process.env.DB_USER,
         password: process.env.DB_PASS,
       });
-      sequelize.addModels([ProductoSchema]);
+      sequelize.addModels([ProductoSchema, AlquilerSchema, AlquilerProductoSchema]);
       await sequelize.sync();
       return sequelize;
     },

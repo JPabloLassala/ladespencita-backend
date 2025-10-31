444edf15fd9637334aacb9618ee8ad09f2844c88
JPabloLassala <jpablolassala@gmail.com>
2025-10-31 17:29:10 -0300

    Enable mongodb debug support

--- src/Database/database.provider.ts
+++ src/Database/database.provider.ts
@@ -4,6 +4,12 @@ import { env } from "process";
 export const databaseProviders = [
   {
     provide: "DATABASE_CONNECTION",
-    useFactory: (): Promise<typeof mongoose> => mongoose.connect(env.MONGO_URL),
+    useFactory: (): Promise<typeof mongoose> => {
+      console.log(`Mongo debug is ${env.MONGO_DEBUG}`);
+      mongoose.set("debug", Boolean(env.MONGO_DEBUG));
+      return mongoose.connect(
+        env.MONGO_URL || "mongodb://root:example@db:27017/nest?authSource=admin",
+      );
+    },
   },
 ];

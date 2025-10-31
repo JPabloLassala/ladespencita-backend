15591329f0c0bc93b6302c94659c2815eff88e40
JPabloLassala <jpablolassala@gmail.com>
2025-10-31 17:29:12 -0300

    Remove ImagenProducto module

--- src/ImagenProducto/ImagenProducto.controller.ts
+++
@@ -1,4 +0,0 @@
-import { Controller } from "@nestjs/common";
-
-@Controller("imagen-producto")
-export class ImagenProductoController {}
--- src/ImagenProducto/index.ts
+++
@@ -1,5 +0,0 @@
-export * from "./ImagenProducto.controller";
-export * from "./ImagenProducto.module";
-export * from "./ImagenProducto.repository";
-export * from "./ImagenProducto.schema";
-export * from "./ImagenProducto.service";

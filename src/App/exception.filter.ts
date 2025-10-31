8fe05858a61bd7600c2c730ce3b6432d546739d0
JPabloLassala <jpablolassala@gmail.com>
2025-10-31 17:29:11 -0300

    Include message in error

--- src/App/exception.filter.ts
+++ src/App/exception.filter.ts
@@ -22,6 +22,7 @@ export class AllExceptionsFilter implements ExceptionFilter {
       statusCode: httpStatus,
       timestamp: new Date().toISOString(),
       path: httpAdapter.getRequestUrl(ctx.getRequest()),
+      message: exception instanceof Error ? exception.message : exception,
     };
 
     this.logger.error(exception);

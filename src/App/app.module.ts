606e85857a1ba96d3bf9d09eb4ad04204f5f345a
JPabloLassala <jpablolassala@gmail.com>
2025-11-05 18:02:15 -0300

    Remove appService

--- src/App/app.module.ts
+++ src/App/app.module.ts
@@ -1,19 +1,12 @@
 import { Module } from "@nestjs/common";
 import { AlquilerModule } from "src/Alquiler";
 import { ProductoModule } from "src/Producto";
-import { AppService } from "./app.service";
 import { ConfigModule } from "@nestjs/config";
 import { AlquilerProductoModule } from "src/AlquilerProducto";
 import { AuthModule } from "src/Auth";
 import { ImageModule } from "src/Image";
 import { DatabaseModule } from "src/Database";
-
-const getPhotoModule = () => {
-  return ServeStaticModule.forRoot({
-    rootPath: join(__dirname, "../../..", "images"),
-    serveRoot: "/images",
-  });
-};
+import { SheetsModule } from "src/Sheet";
 
 @Module({
   imports: [
